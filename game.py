38402c837938fd62474b88202786b134a8851621
aredfern15 <77812013+aredfern15@users.noreply.github.com>
2021-02-05 17:13:08 -0500

    Validate user inputs

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 
 
 import random
+#from random import choice
 
 print("-------------------")
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
@@ -16,6 +17,26 @@ user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
 print(f"You chose: {user_choice}")
 
 #
+# validate the user selection
+#
+# stop the program (not try to determine the winner)
+#... if the user choice is invalid 
+
+#user_choice.lower()
+
+#if user_choice in options: 
+ #   #print("GOOD")
+ #   pass
+#else: 
+ #   print("OOPS, please choose a valid option and try again")
+  #  exit()
+
+options = ["rock", "paper", "scissors"]
+if user_choice not in options: 
+    print("OOPS, please choose a valid option and try again")
+    exit()
+
+#
 #simulating a computer input 
 #
 
@@ -25,23 +46,42 @@ print(f"You chose: {user_choice}")
 #foo = ['a', 'b', 'c', 'd', 'e']
 #computer_choice = random.choice(foo)
 
-options = ["rock", "paper", "scissors"]
+
 computer_choice = random.choice(options)
-
-
+#computer_choice = choice(options)
 
 print(f"The computer chose: {computer_choice}")
-#
-
-
-
-
 
 #
 #determining who won
 #
 
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("Oh, it's a tie.")
+    elif computer_choice == "paper":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "scissors":
+        print("Oh, you won! Nice job.")
 
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "paper":
+        print("Oh, it's a tie.")
+    elif computer_choice == "scissors":
+        print("Oh, the computer won. It's ok.")
+
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "paper":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "scissors":
+        print("Oh, it's a tie.")
+
+else:
+    print("OOPS SOMETHING WENT WRONG.")
 
 print("-------------------")
 print("Oh, the computer won. It's ok.")
